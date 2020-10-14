32a1f99864a1ae145a856d68fa079990abee9aff
PA1n7 <64380382+PA1n7@users.noreply.github.com>
2020-10-14 12:47:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(window.location);
+function parseParams(search){let res = search.replace("?", ""); return res;}
+
+let url = parseParams(window.location.search)
+
+if{
+  url.includes("&")){let urlList = url.split("&"); 
+}
+
+try {
+  console.log(urlList)
+}catch{
+  vars[url.split("=")[0]] = url.split("=")[1]
+}
+
+let name = vars["name"];
+
+console.log(vars.name);
